73b0aebb675cb60f0adabf14460ca64c97168c0e
GargiJain206 <gargijain7582@gmail.com>
2025-08-24 16:43:53 +0530

    Add files via upload

---
+++ maximum.py
@@ -0,0 +1,6 @@
+#Read three integers and print their maximum. 
+a = float(input("Enter first number"))
+b = float(input("Enter second number"))
+c = float(input("Enter third number"))
+maximum = max(a,b,c)
+print("The maximum number is : " ,maximum)
