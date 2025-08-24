bf742a7fa58b9c36ed13c75d3fc9045903e70d71
GargiJain206 <gargijain7582@gmail.com>
2025-08-24 16:49:41 +0530

    Add files via upload

---
+++ maximumamong2.py
@@ -0,0 +1,5 @@
+#Write a program to input two numbers(A & B) from the user and print the maximum element among A & B. 
+A = int(input("Enter first number "))
+B = int(input("Enter second number "))
+maximum = max(A,B)
+print("The maximum element is",maximum)
