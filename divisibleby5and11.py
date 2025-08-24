569745cf8089a29eb3d61e867eddf09892dacc1d
GargiJain206 <gargijain7582@gmail.com>
2025-08-24 16:43:20 +0530

    Add files via upload

---
+++ divisibleby5and11.py
@@ -0,0 +1,6 @@
+#Take an integer A as input. You have to tell whether A is divisible by both 5 and 11 or not. 
+A = int(input("Enter the number"))
+if A%5==0 and A%11==0:
+    print("Condition is true")
+else:
+    print("Condition is false")    
